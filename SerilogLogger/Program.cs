95e59bfaec61a0a51014a461ca9cc99ba1b53de2
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-08-20 18:36:08 +0300

    Serilog logger introduction

---
+++ SerilogLogger/Program.cs
@@ -0,0 +1,10 @@
+﻿using Serilog;
+
+// Packages
+// For logger creation  > Serilog 
+// For console provider > Serilog.Sinks.Console
+
+Serilog.Core.Logger logger = new LoggerConfiguration()
+       .WriteTo.Console().CreateLogger();
+
+logger.Information("This is an information log");
