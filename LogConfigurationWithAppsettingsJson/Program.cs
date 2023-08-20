421f0cdaa1f8dd29346d77c0c81d412b4c0aef1d
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-08-20 15:17:48 +0300

    Log configurations from appsettings json

---
+++ LogConfigurationWithAppsettingsJson/Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurations are coming from appsettings.json
+// Default config customized
+builder.Services.AddLogging();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+app.MapGet("/weatherforecast", (ILogger<WeatherForecast> logger) =>
+{
+    logger.LogInformation("Weather forecast retrieve started");
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+
+    logger.LogWarning("Weather forecast retrieved");
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
