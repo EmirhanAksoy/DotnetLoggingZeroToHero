4ccbc9235d91f941ffb407ea25389b62a81fd3e2
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-08-20 21:30:20 +0300

    Serilog enrichers

---
+++ SerilogLogEnrichment/Program.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+
+
+Serilog.ILogger logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .Enrich.WithMachineName()
+    .Enrich.WithThreadId()
+    .Enrich.WithProperty("Kiosk","KioskNo1") // custom enricher
+    .CreateLogger();
+
+Log.Logger = logger;
+
+
+// In this example log echriched by WithMachineName and WithThreadId methods
+
+// Need to install Enricher packages separately
+
+//Packages :
+// Serilog.Enrichers.Thread > for thread id
+// Serilog.Enrichers.Environment > for machine name
+
+logger.Information("This is an information log Machine :{MachineName} ThreadId:{ThreadId} Kiosk:{Kiosk} ");
+
+
