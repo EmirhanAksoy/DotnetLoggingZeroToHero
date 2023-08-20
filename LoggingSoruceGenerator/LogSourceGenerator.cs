8966fca28e8a04f925ba4fd3361c038c8f52822d
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-08-20 22:28:11 +0300

    Logging source generators

---
+++ LoggingSoruceGenerator/LogSourceGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LoggingSoruceGenerator;
+
+public static partial class LoggerExtensions
+{
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        EventId = 5001,
+        Message = "Payment with id:{paymentId} and amount:{amount} has been successfully completed"
+        )]
+    public static partial void LogPayment(this ILogger logger,string paymentId,decimal amount);
+}
---
+++ LoggingSoruceGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using LoggingSoruceGenerator;
+using Microsoft.Extensions.Logging;
+
+
+using ILoggerFactory loggerFactory = LoggerFactory.Create((configure) =>
+{
+    configure.AddConsole();
+});
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+// No need to write this actions all the time because source generators help us 
+
+// Check LoggingSoruceGenerator.cs
+//Action<Microsoft.Extensions.Logging.ILogger, string, decimal, Exception?> logPayment =
+//    LoggerMessage.Define<string, decimal>(
+//        LogLevel.Information,
+//        new EventId(5502, nameof(Payment)),
+//        "Payment with id:{paymentId} and amount:{amount} has been successfully completed");
+
+
+logger.LogPayment(Guid.NewGuid().ToString(), 5541.65m);
+
+public class Payment
+{
+    public Payment(decimal amount)
+    {
+        this.Id = Guid.NewGuid().ToString();
+        this.Amount = amount;
+    }
+    public string Id { get; set; }
+    public decimal Amount { get; set; }
+}
