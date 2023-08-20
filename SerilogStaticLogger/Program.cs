d0c9501e5490945af1ba57c77ddb824aefd0771e
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-08-20 18:40:51 +0300

    Serilog static logger

---
+++ SerilogStaticLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using Serilog;
+
+Serilog.Core.Logger logger = new LoggerConfiguration()
+       .WriteTo.Console().CreateLogger();
+
+// Serilog has also static class to log
+// It is gloabally-shared logger
+// We're setting gloabally-shared logger with our configured logger
+// Without this line no log will be displayed
+Log.Logger = logger;    
+
+Log.Information("This is an information log");
