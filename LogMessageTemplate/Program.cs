29250b8541875af6d09a58bc9d7ec38a643a91c1
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-08-20 17:54:15 +0300

    Log message formatting

---
+++ LogMessageTemplate/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+using ILoggerFactory loggerFactory = LoggerFactory.Create((builder) =>
+{
+    builder.AddConsole();
+    builder.SetMinimumLevel(LogLevel.Information);
+});
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+decimal totalCost = 56.8m;
+DateTime now = DateTime.UtcNow;
+
+logger.LogInformation("Total cost is {totalCost:000} | {date:HH:mm:ss}", totalCost, now);
+
+// While logging we can use formatters to display messages properly
