77f2cc94c8b07c0883b3118e8bca09ed07da0c63
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-08-20 13:12:58 +0300

    Log categories

---
+++ LogCategories/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+
+using ILoggerFactory loggerFactory = LoggerFactory.Create((builder) =>
+{
+    builder.AddConsole();
+    builder.SetMinimumLevel(LogLevel.Information); 
+});
+
+// Log Category is logical way to group different log entires
+
+
+// No category provided
+ILogger loggerWithoutCategory = loggerFactory.CreateLogger<Program>();
+loggerWithoutCategory.LogInformation("Logger without category");
+
+//OUTPUT :
+//info: Program[0]
+//      Logger without category
+
+ILogger userLogCategory = loggerFactory.CreateLogger<UserLogCategory>();
+userLogCategory.LogInformation("User log category");
+
+//OUTPUT :
+//info: UserLogCategory[0]
+//      User log category
+
+ILogger paymentLogCategory = loggerFactory.CreateLogger<PaymentLogCategory>();
+paymentLogCategory.LogInformation("Payment log category");
+
+//OUTPUT :
+//info: PaymentLogCategory[0]
+//      Payment log category
+
+
+// On controller levels ILogger can be injected as ILogger<ControllerClassName> to use log categories
+
+public class UserLogCategory { }
+
+public class PaymentLogCategory { }
