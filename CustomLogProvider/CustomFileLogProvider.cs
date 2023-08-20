391890fe81c2c0a9cc2c940b12b4425381e79e9b
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-08-20 17:03:22 +0300

    Custom log provider

---
+++ CustomLogProvider/CustomFileLogProvider.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace CustomLogProvider;
+
+public class CustomFileLogProvider : ILoggerProvider, IDisposable
+{
+    private readonly string _filePath;
+    public CustomFileLogProvider(string path)
+    {
+        _filePath = path;
+    }
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new CustomFileLogger(_filePath);
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
+
+public class CustomFileLogger : ILogger
+{
+    private readonly string _filePath;
+    public CustomFileLogger(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public IDisposable BeginScope<TState>(TState state) where TState : notnull
+    {
+        return NullScope.Instance;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel >= LogLevel.Debug;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        if (IsEnabled(logLevel))
+        {
+            string exceptionMessage = exception is not null ? JsonSerializer.Serialize(exception) : "-";
+            File.AppendAllLines(_filePath,new List<string>()
+            {
+               $"Log Level:{nameof(logLevel)} | EventId:{eventId} | Exception:{exceptionMessage} | Message:{state} | Time:{DateTime.UtcNow}"
+            });
+        }
+    }
+}
+
+public static class LoggerFactoryExtensions
+{
+    public static void AddCustomFileLog(this ILoggingBuilder loggingBuilder, string path)
+    {
+        loggingBuilder.AddProvider(new CustomFileLogProvider(path));
+    }
+}
+
+internal sealed class NullScope : IDisposable
+{
+    public static NullScope Instance { get; } = new();
+    public NullScope()
+    {
+            
+    }
+    public void Dispose()
+    {
+        
+    }
+}
---
+++ CustomLogProvider/Program.cs
@@ -0,0 +1,17 @@
+﻿using CustomLogProvider;
+using Microsoft.Extensions.Logging;
+
+using ILoggerFactory loggerFactory = LoggerFactory.Create((builder) =>
+{
+    builder.AddConsole();
+    builder.AddCustomFileLog("Log.txt");
+    builder.SetMinimumLevel(LogLevel.Information);
+});
+
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+logger.LogInformation("Hello world !");
+
+// Check the log file > \CustomLogProvider\bin\Debug\net7.0>Log.txt
+
